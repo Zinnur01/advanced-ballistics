2eea59c90d6f57705646b657a9816cbca174b3b2
Shacall <davleev_zinnur@mail.ru>
2022-03-09 17:40:22 +0300

    add camera and player movement

---
+++ Assets/Core/Runtime/CameraMovement.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField]
+    private Transform cameraHinge;
+
+    [SerializeField]
+    private Vector2 sensetivity;
+
+    // Stored required properties.
+    private float yAxis;
+
+    private void Update()
+    {
+        float x = Input.GetAxis("Mouse X") * sensetivity.x * Time.deltaTime;
+        float y = Input.GetAxis("Mouse Y") * sensetivity.y * Time.deltaTime;
+
+        transform.Rotate(new Vector3(0, x, 0));
+
+        yAxis -= y;
+        yAxis = Mathf.Clamp(yAxis, -90, 90);
+
+        cameraHinge.localRotation = Quaternion.Euler(yAxis, 0, 0);
+    }
+}
---
+++ Assets/Core/Runtime/PlayerMovement.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 5;
+
+    // Stored required components.
+    private CharacterController controller;
+
+    private void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        Vector3 moveVector = transform.forward * y + transform.right * x;
+
+        controller.Move(moveVector * speed * Time.deltaTime);
+    }
+}
