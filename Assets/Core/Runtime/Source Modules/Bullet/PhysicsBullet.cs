5db94da75bc0cf1a6f476d65dc556a463775755b
Shacall <davleev_zinnur@mail.ru>
2022-03-12 11:11:56 +0300

    update bullets and add decals

--- Assets/Core/Runtime/Source Modules/Bullet/PhysicsBullet.cs
+++ Assets/Core/Runtime/Source Modules/Bullet/PhysicsBullet.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-//[RequireComponent(typeof(Rigidbody))]
 [RequireComponent(typeof(SphereCollider))]
 public class PhysicsBullet : PoolObject
 {
@@ -21,7 +20,8 @@ public class PhysicsBullet : PoolObject
     [SerializeField]
     private float airResistance = 0.1f;
 
-    // Stored required components.
+    [SerializeField]
+    private PoolObject decalTemplate;
 
     // Stored required properties.
     [SerializeField]
@@ -30,13 +30,38 @@ public class PhysicsBullet : PoolObject
 
     private Vector3 lastPosition;
 
+    private void OnEnable()
+    {
+        lastPosition = transform.position;
+    }
+
+
     private void FixedUpdate()
     {
+        //Vector3 reflection = Vector3.Reflect(velocity, -Vector3.ProjectOnPlane(velocity, Vector3.up).normalized);
+        //Debug.DrawRay(transform.position, reflection * airResistance);
+        //velocity += reflection * airResistance * Time.fixedDeltaTime;
+
         velocity += Physics.gravity * Time.fixedDeltaTime;
+
+        if (Physics.Linecast(lastPosition, transform.position, out RaycastHit hitInfo))
+        {
+            PoolManager.Instance.CreateOrPop<Decal>(decalTemplate, hitInfo.point, Quaternion.LookRotation(-hitInfo.normal, Vector3.up));
+            transform.position = hitInfo.point;
+            velocity = Vector3.Reflect(velocity, hitInfo.normal);
+            //Push();
+        }
+
         transform.Translate(velocity * Time.fixedDeltaTime, Space.World);
 
         Debug.DrawLine(lastPosition, transform.position, Color.red, 5f);
+
         lastPosition = transform.position;
+    }
+
+    protected override void OnBeforePush()
+    {
+        lastPosition = Vector3.zero;
     }
 
     public void ApplyVelocity(Vector3 vector)
